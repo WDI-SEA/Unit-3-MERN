8557f762d1b8f9fd1bd69dddda047eaf557d2bfb
Fuad AlShuroogi <fuadalshuroogi@gmail.com>
2022-11-02 23:59:27 +0300

    Done with task1-Simpsons

--- Props-Simpsons-Website-Lab/src/components/ProfileCard/ProfileCard.jsx
+++ Props-Simpsons-Website-Lab/src/components/ProfileCard/ProfileCard.jsx
@@ -7,14 +7,12 @@ export default function ProfileCard(props) {
                 <img src={props.pic} className="profile-image" alt='' />
                 <div className="single-content">
                     <div className="card-middle">
-                        <h1>Name Here</h1>
-                        <p className="team-p1">
-                            Description here description here description here description here description here description here description here description here description here description here description here description here description here ... 
-                        </p>
+                        <h1>{props.name}</h1>
+                        <p className="team-p1">{props.description}</p>
                     </div>
                     <div className="card-bottom">
                         <div className="card-email">
-                            insert@email.here
+                            {props.email}
                         </div>
                         <div className="card-icon profile-trigger" data-id="single-article-1" >
                             <i className="fa fa-chevron-right"></i>
