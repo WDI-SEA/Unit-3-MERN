ab5f1adb9bc971dede2e3ecd13259c4dbd0f5c91
Anwaar <anwaaralsyouf@gmail.com>
2022-11-03 20:57:18 +0300

    Routes and Links added!

--- React-Router-V6-Portfolio-HW/src/App.js
+++ React-Router-V6-Portfolio-HW/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-//import all of your components to use them
-
-
-//Guide: In your App.js return below: 
-// return (
-//   <div className="App">
-  //Put your nav bar first with <Link> components
-  //Put your <Route> components after
-//   </div>
-// );
-
+import "./App.css";
+import About from "./components/About/About.jsx";
+import ContactMe from "./components/ContactMe/ContactMe";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
+import SignUp from "./components/SignUp/SignUp";
+import Skills from "./components/Skills/Skills";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-    
+      <Router>
+        {/* The Links: */}
+        <nav>
+            <Link to="/signUp">SignUp</Link> &nbsp; 
+            <Link to="/login">Login</Link> &nbsp;
+            <Link to="/profile">Profile</Link> &nbsp;
+            <Link to="/skills">Skills</Link> &nbsp;
+            <Link to="/about">About</Link> &nbsp;
+            <Link to="/contactMe">ContactMe</Link> &nbsp;
+
+        </nav>
+
+        {/* The Routes: */}
+        <Routes>
+          <Route path="/signUp" element={<SignUp />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/skills" element={<Skills />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contactMe" element={<ContactMe />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+//Done By: Anwaar®.
---
+++ React-Router-V6-Portfolio-HW/src/components/ContactMe/ContactMe.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ContactMe() {
+  return (
+    <div>
+      <h1>ContactMe</h1>
+    </div>
+  );
+}
+
+export default ContactMe;
