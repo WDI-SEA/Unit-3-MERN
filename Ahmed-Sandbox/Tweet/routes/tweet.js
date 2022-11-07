227b7ef0d0332005b4e52f0f8f9cd5a0be47604d
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-11-07 10:44:17 +0300

    something

--- Ahmed-Sandbox/Tweet/routes/tweet.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const tweetController = require('../controllers/tweet')
-
-router.get('/', function(req, res, next) {
-  res.redirect('/movies');
-});
-
-module.exports = router;
--- Ahmed-Sandbox/Tweet/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-require('./config/database')
-
-/*
-    Create a Tweet model with the following string properties:
-    name
-    description
-    Begin creating basic CRUD with restful routes for the Tweet model
-*/
