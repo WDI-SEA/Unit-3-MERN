1a797cd74feb653e5f944bf49998aada3bba42d3
Kareem <kareem@Kareems-MacBook-Pro.local>
2022-11-08 13:07:49 +0300

    Inital React frontend application

---
+++ FullStack-Twitter-Instructor/frontend/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+// - Home Page:
+// - Form to create tweet -> onChange, onSubmit, store form data in state, API call
+
+function HomePage() {
+  //GOAL - display all tweets below
+    // STEP 1: API call to our backend (
+        // useEffect
+        // axios.get(.. ..localhost:4000/tweets) -> returns json data
+    // STEP 2: Take the tweet list data and store it in our state
+        // Take response -> setState(response)
+    // STEP 3: Display data in HTML using tweets.map .. .. 
+
+  return (
+    <div>
+    {/* Form - 1 Input -> content */}
+
+    {/* tweets[].map( .. .. .) */}
+    </div>
+  )
+}
+export default HomePage
