9dd491f68abb2206c901189ee0d0fb09f4549f4e
saadkhan29 <saad.iqbal@generalassemb.ly>
2022-11-09 12:01:57 +0300

    Auth updated - Saad Iqbal

--- FullStack-Twitter-Instructor/backend/controllers/users.js
+++ FullStack-Twitter-Instructor/backend/controllers/users.js
@@ -1,20 +1,73 @@
 const User = require('../models/User')
 const Tweet = require('../models/Tweet')
 
+// Require bcrypt 
+const bcrypt = require('bcrypt');
+const salt = 10;
+
+// Require jsonwebtoken
+const jwt = require("jsonwebtoken");
 
 async function createUser(req,res) {
     try {
+        // Plain Text to Ecrypted String
+        let hashedPassword = bcrypt.hashSync(req.body.password, salt)
+        console.log(hashedPassword);
+
         const newUser = await User.create({
             name: req.body.name,
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password,
+            password: hashedPassword,
         })
         res.json(newUser)
     } catch (err) {
         res.json(err)
     }
 }
+
+const auth_sigin_post = async (req, res) =>{
+    let {email, password} = req.body;
+    console.log(email);
+
+    try{
+        let user = await User.findOne({email}); // email: email
+        console.log(user);
+
+        if(!user){
+            return res.json({message: "User not found"}).status(400);
+        }
+
+        // Password Comparison
+        const isMatch = await bcrypt.compareSync(password, user.password);
+        console.log(password); // Plaintext password
+        console.log(user.password); // Encrypted password
+
+        if(!isMatch) {
+            return res.json({message: "Password not matched"}).status(401);
+        }
+
+        // JWT Token
+        const payload = {
+            user: {
+                id: user._id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            process.env.SECRET,
+            { expiresIn: 36000000},
+            (err, token) => {
+                if(err) throw err;
+                res.json({token}).status(200);
+            }
+        )
+    } catch (error) {
+        console.log(error)
+        res.json({message: "You are not loggedin!. Try again later."}).status(400);
+    }
+} 
 
 async function createUserTweet(req,res) {
     // Find the user that created the tweet
@@ -69,5 +122,6 @@ module.exports = {
     getAllUsers,
     createUserTweet,
     updateUser,
-    deleteUser
+    deleteUser,
+    auth_sigin_post
 }
